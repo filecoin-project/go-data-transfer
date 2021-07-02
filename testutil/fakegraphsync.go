2b03c2747d908d0e48769dee88ee5e9d9ef33986
hannahhoward <hannah@hannahhoward.net>
2021-07-02 14:55:57 -0700

    Revert "Fire a transfer queued event when a transfer is queued in Graphsync (#221)"
    This reverts commit 7794046e5c17b37a7792a3cf643893faad5431ee.

--- testutil/fakegraphsync.go
+++ testutil/fakegraphsync.go
@@ -106,7 +106,6 @@ type FakeGraphSync struct {
 	OutgoingRequestHook          graphsync.OnOutgoingRequestHook
 	IncomingBlockHook            graphsync.OnIncomingBlockHook
 	OutgoingBlockHook            graphsync.OnOutgoingBlockHook
-	IncomingRequestQueuedHook    graphsync.OnIncomingRequestQueuedHook
 	IncomingRequestHook          graphsync.OnIncomingRequestHook
 	CompletedResponseListener    graphsync.OnResponseCompletedListener
 	RequestUpdatedHook           graphsync.OnRequestUpdatedHook
@@ -284,14 +283,6 @@ func (fgs *FakeGraphSync) RegisterIncomingRequestHook(hook graphsync.OnIncomingR
 	fgs.IncomingRequestHook = hook
 	return func() {
 		fgs.IncomingRequestHook = nil
-	}
-}
-
-// RegisterIncomingRequestQueuedHook adds a hook that runs when an incoming GS request is queued.
-func (fgs *FakeGraphSync) RegisterIncomingRequestQueuedHook(hook graphsync.OnIncomingRequestQueuedHook) graphsync.UnregisterHookFunc {
-	fgs.IncomingRequestQueuedHook = hook
-	return func() {
-		fgs.IncomingRequestQueuedHook = nil
 	}
 }
 
